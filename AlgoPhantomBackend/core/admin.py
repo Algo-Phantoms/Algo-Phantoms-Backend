405f9ad3b5b55db7cb5c35600940aa38aefae9fb
Shagun Jain <jshagun710@gmail.com>
2021-03-17 10:23:50 +0530

    add quizscore model

--- AlgoPhantomBackend/core/admin.py
+++ AlgoPhantomBackend/core/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from .models import *
 from django.contrib.auth.models import User
 # Register your models here.
+admin.site.register(quizScore)
--- AlgoPhantomBackend/core/models.py
+++ AlgoPhantomBackend/core/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from quiz.models import Quizzes
 # Create your models here.
 
+class quizScore(models.Model):
+	score = models.IntegerField(default=0)
+	maximum_score = models.IntegerField(default=0)
+	is_cleared = models.BooleanField(default=False)
+	user = models.ForeignKey('auth.User', related_name='quizscore', on_delete=models.CASCADE, blank=True, null=True)
+	quiz = models.ForeignKey(Quizzes, related_name='quizscore', on_delete=models.CASCADE, blank=True, null=True)
