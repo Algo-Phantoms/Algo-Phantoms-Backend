37cf2e5bb5396dd0ec3ddcd599652d900cfbaf20
Arif <arif.asan.jmi@gmail.com>
2021-04-02 09:35:29 +0530

    Local_settings resolved

--- AlgoPhantomBackend/AlgoPhantomBackend/settings.py
+++ AlgoPhantomBackend/AlgoPhantomBackend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# from .local_settings import *
+from .local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -162,6 +162,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = EMAIL_HOST_USER
-# EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
