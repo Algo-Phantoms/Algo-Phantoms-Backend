f5cd13ef57783df5c9b325b56ebc7aa076d2b551
somya51p <somya2001p@gmail.com>
2021-04-06 13:20:55 +0530

    change password functionality added

--- AlgoPhantomBackend/core/urls.py
+++ AlgoPhantomBackend/core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import ChangePasswordView
 from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    # Change Password
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
 ]
--- AlgoPhantomBackend/core/views.py
+++ AlgoPhantomBackend/core/views.py
@@ -1,8 +1,47 @@
 from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.http import HttpResponseBadRequest, JsonResponse,HttpResponseRedirect
 from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.response import Response
+from .serializers import ChangePasswordSerializer
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 def home(request):
     return render(request, 'index.html')
 
+
+class ChangePasswordView(generics.UpdateAPIView):
+    """
+    An endpoint for changing password.
+    """
+    serializer_class = ChangePasswordSerializer
+    model = User
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            # Check old password
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response({"old_password": ["Wrong password.Try again"]}, status=status.HTTP_400_BAD_REQUEST)
+
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password updated successfully',
+                'data': []
+            }
+
+            return Response(response)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
